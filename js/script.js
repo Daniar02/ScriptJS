b332803e2ce146b53808a55beadf6885db99c6d7
Daniar02 <abykeev02@icloud.com>
2023-01-12 14:12:52 +0600

    Простое общение с пользователем

--- js/script.js
+++ js/script.js
@@ -101,3 +101,19 @@
 // };
 // // console.log(obj.alex);
 // // let user = new Object(); // синтаксис "конструктор объекта"
+
+// ######################################## Простое общение с пользователем ########################################
+// const sd = alert("Hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18", 18);
+// console.log(typeof(answer)); // (строка)
+
+// const answers = [];
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваше фамиля?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+// document.write(answers);
+// console.log(typeof(answers));
