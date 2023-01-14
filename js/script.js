a4eb5b0e9062a979e5b903d590259159f799fe8a
Daniar02 <abykeev02@icloud.com>
2023-01-14 09:30:38 +0600

    Задачи на логические операторы

--- js/script.js
+++ js/script.js
@@ -239,3 +239,70 @@
 //         break;
 // }
 
+// ############################################# Логические операторы #############################################
+// const hamburger = 5;
+// const fries = null;
+
+// if (hamburger && fries) {
+//     console.log("Я сыть!");
+// }
+// console.log((hamburger && fries));
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+// console.log((hamburger === 3 && cola && fries));
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && "dssd");
+// if (hamburger === 3 && cola && fries) {
+//     console.log("Все сыты!");
+// } else {
+//     console.log("Мы уходим");
+// }
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 3;
+// // console.log((hamburger === 3 && cola && fries));
+
+// if (hamburger === 3 && (cola === 2 || fries === 3) && nuggets) {
+//     console.log("Все сыты!");
+// } else {
+//     console.log("Мы уходим");
+// }
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+// console.log(!0);
+// console.log((hamburger || cola || fries));
+
+// let johnReport, alexReport, samReport, mariaReport = "done";
+// console.log(johnReport || alexReport || samReport || mariaReport);
+
+// console.log( NaN || 2 || undefined );
+// console.log( NaN && 2 && undefined );
+// console.log( 1 && 2 && 3 );
+// console.log( !1 && 2 || !3 );
+// console.log( 25 || null && !3);
+// console.log( NaN || null || !3 || undefined || 5);
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Сделано');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Сделано!');
+// }
