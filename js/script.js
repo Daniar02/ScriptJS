a4601753579b2ae1a2d9204275f095d304122f69
Daniar02 <abykeev02@icloud.com>
2023-01-18 22:51:05 +0600

    Передача по ссылке или по значению, Spread оператор (ES6-ES9)

--- js/script.js
+++ js/script.js
@@ -1050,3 +1050,86 @@ P.S. Функции вызывать не обязательно*/
 // const products = str.split(','); // Метод split() возвращает новый массив.
 // products.sort();
 // console.log(products.join("; "));
+
+// ########################## Передача по ссылке или по значению, Spread оператор (ES6-ES9) ##########################
+
+let a = 5,
+    b = a;
+b = b + 5;
+console.log(b);
+console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+const copy = obj; // Сылку
+copy.a = 10;
+console.log(copy);
+console.log(obj);
+
+function copy(mainObj) {
+    let objCopy = {};
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+// console.log(newNumbers);
+// console.log(numbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+// console.log(Object.assign(numbers, add));
+
+const clone = Object.assign({}, add);
+clone.d = 20;
+console.log(add);
+console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+newArray[1] = 'dancho';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger', 'rutube'],
+      internet = [...video, ...blogs, 'vk', 'foocebuc'];
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+const num = [2, 5, 7];
+log(...num);
+
+const array = ['a', 'b'];
+const newAarray = [...array];
+console.log(newAarray);
+
+const q = {
+    one: 1,
+    two: 2
+};
+const newObj = {...q};
+console.log(newObj);
