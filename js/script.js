e5908b2ad320898900eb366d8ec53e0eb6b0e444
Daniar02 <abykeev02@icloud.com>
2023-01-24 22:34:25 +0600

    Итерации в массивах filter map уп

--- js/script.js
+++ js/script.js
@@ -1689,3 +1689,25 @@ P.S. Функции вызывать не обязательно*/
 //     }
 // }
 // console.log(positiveOperations);
+///////////////////// filter ////////////////////////
+// const positiveOperations = operations.filter(operation => {
+//     return operation > 0;
+// });
+// console.log(positiveOperations);
+// const positiveRUBOperations = operations.filter(operation => {
+//     return operation > 0;
+//     })
+//     .map(operation => operation * 60);
+// console.log(positiveRUBOperations);
+///////////////////// Упражнения ////////////////////////
+// const prices = [[100, 200], [120,100], [200,350]];
+// const result = prices.map(product => product[1] - product[0])
+//                      .filter(price => price > 0);
+// console.log(result);
+////////////////////////////////////////////
+// const operations = [100, -20, 7, -30, 50];
+// let balance = 0;
+// for (const operation of operations) {
+//     balance += operation;
+// } 
+// console.log(balance); 
