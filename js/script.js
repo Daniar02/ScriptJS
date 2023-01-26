73d9c7e064c699eb01dd6b588fb22e2499a64d0a
Daniar02 <abykeev02@icloud.com>
2023-01-26 16:39:14 +0600

    Динамическая типизация в JS

--- js/script.js
+++ js/script.js
@@ -1984,5 +1984,47 @@ P.S. Функции вызывать не обязательно*/
 // }
 // console.log(sorted);
 
+// ##########################################  Динамическая типизация в JS ##########################################
 
+    // To String
+    // 1)
+    console.log(typeof(String(null)));
+    console.log(typeof(String(4)));
+    // 2)
+    console.log(typeof(null + ''));
+
+    const num = 5;
+
+    console.log("https://vk.com.catalog/" + num);
+
+    const fontSize = 26 + "px";
+    console.log(fontSize);
+
+    // To Number
+    // 1)
+    console.log(typeof(Number('4')));
+    // 2)
+    console.log(typeof(+('4')));
+    // 3)
+    console.log(typeof(parseInt('15px', 10)));
+
+    let answ = +prompt("hello", "");
+
+    // To boolean
+    // false - 0, '', null, undefined, NaN;
+    // 1)
+    let switcher = null;
+
+    if (switcher) {
+        console.log('Working...');
+    }
+    switcher = 1;
+
+    if (switcher) {
+        console.log('Working...');
+    }
+    // 2)
+    console.log(typeof(Boolean('4')));
+    // 3)
+    console.log(typeof((!!'434344')));
     
