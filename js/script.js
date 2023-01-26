14572519f4b5f4014016a28d40c55069ee6733a4
Daniar02 <abykeev02@icloud.com>
2023-01-26 16:38:27 +0600

    Отлавливаем ошибки в своем кода при помощи разработчики. Breakpoints

--- js/script.js
+++ js/script.js
@@ -1890,77 +1890,99 @@ P.S. Функции вызывать не обязательно*/
 
 // Код возьмите из предыдущего домашнего задания
 
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    start: function() {
-        personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', "");
+// const personalMovieDB = {
+//     count: 0,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+//     start: function() {
+//         personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', "");
     
-        while(personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?');
-        }
-    },
-    remeberMyFilms: function() {
-        for (let i = 0; i < 2; i++) {
-            let a = prompt('Один из последних просмотренных фильмов?').trim(),
-                b = prompt('На сколько оцените его?');
-             if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-                 personalMovieDB.movies[a] = b;
-                 console.log('Done');
-             } else {
-                 console.log('error');
-                 i--;
-             } 
-         }
-    },
-    detectPersonalLevel: function() {
-        if (personalMovieDB.count < 10) {
-            console.log("Просмотрено довольно мало фильмов");
-        } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-            console.log("Вы классический зритель");
-        } else if (personalMovieDB.count >= 30 ) {
-            console.log("Вы киноман");
-        } else {
-            console.log("Произошла ошибка");
-        }
-    },
-    showMyDB: function(hidden) {
-        if (!hidden) {
-            console.log(personalMovieDB);
-        }
-    },
-    toggleVisibleMyDB: function() {
-        if (personalMovieDB.privat) {
-            personalMovieDB.privat = false;
-        } else {
-            personalMovieDB.privat = true;
-        }
-    },
-    writeYourGenres: function() {
-        for (let i = 1; i < 2; i++) {
-            // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+//         while(personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+//             personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?');
+//         }
+//     },
+//     remeberMyFilms: function() {
+//         for (let i = 0; i < 2; i++) {
+//             let a = prompt('Один из последних просмотренных фильмов?').trim(),
+//                 b = prompt('На сколько оцените его?');
+//              if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//                  personalMovieDB.movies[a] = b;
+//                  console.log('Done');
+//              } else {
+//                  console.log('error');
+//                  i--;
+//              } 
+//          }
+//     },
+//     detectPersonalLevel: function() {
+//         if (personalMovieDB.count < 10) {
+//             console.log("Просмотрено довольно мало фильмов");
+//         } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+//             console.log("Вы классический зритель");
+//         } else if (personalMovieDB.count >= 30 ) {
+//             console.log("Вы киноман");
+//         } else {
+//             console.log("Произошла ошибка");
+//         }
+//     },
+//     showMyDB: function(hidden) {
+//         if (!hidden) {
+//             console.log(personalMovieDB);
+//         }
+//     },
+//     toggleVisibleMyDB: function() {
+//         if (personalMovieDB.privat) {
+//             personalMovieDB.privat = false;
+//         } else {
+//             personalMovieDB.privat = true;
+//         }
+//     },
+//     writeYourGenres: function() {
+//         for (let i = 1; i < 2; i++) {
+//             // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
 
-            // if (genre === '' || genre === null) {
-            //     console.log('Вы ввели некорректные данные или  ввели их вообще');
-            //     i--;
-            // } else {
-            //     personalMovieDB.genres[i - 1] = genre;
-            // }
-            let genres = prompt(`Введите ваши любимые жанры через запятую`).toLocaleLowerCase();
-            if (genres === '' || genres === null) {
-                console.log('Вы ввели некорректные данные или  ввели их вообще');
-                i--;
-            } else {
-                personalMovieDB.genres = genres.split(', ');
-                personalMovieDB.genres.sort();
-            }
-        }
-        personalMovieDB.genres.forEach((item, i) => {
-            console.log(`Любимый жанр ${i + 1} - это ${item}`);
-        });
+//             // if (genre === '' || genre === null) {
+//             //     console.log('Вы ввели некорректные данные или  ввели их вообще');
+//             //     i--;
+//             // } else {
+//             //     personalMovieDB.genres[i - 1] = genre;
+//             // }
+//             let genres = prompt(`Введите ваши любимые жанры через запятую`).toLocaleLowerCase();
+//             if (genres === '' || genres === null) {
+//                 console.log('Вы ввели некорректные данные или  ввели их вообще');
+//                 i--;
+//             } else {
+//                 personalMovieDB.genres = genres.split(', ');
+//                 personalMovieDB.genres.sort();
+//             }
+//         }
+//         personalMovieDB.genres.forEach((item, i) => {
+//             console.log(`Любимый жанр ${i + 1} - это ${item}`);
+//         });
     
-    }
-};
+//     }
+// };
+
+
+// ##################  Отлавливаем ошибки в своем коде при помощи консоли разработчика. Breakpoints ##################
+// function hello() {
+//     console.log('Hello World');
+// }
+// hello();
+
+// function hi() {
+//     console.log('Say hi!');
+// }
+// hi();
+
+// const arr = [1, 14, 4, 30, 54],
+//       sorted = arr.sort(compareNun);
+// function compareNun(a, b) {
+//     return a - b;
+// }
+// console.log(sorted);
+
+
+    
