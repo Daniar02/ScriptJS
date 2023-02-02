ea71c0bd4e7c7144563d0bff16f0005762eefeb2
Daniar02 <abykeev02@icloud.com>
2023-02-02 08:55:47 +0600

    Тип данных Symbol

--- js/script.js
+++ js/script.js
@@ -2434,30 +2434,68 @@ P.S. Функции вызывать не обязательно*/
 // console.log(userData?.skills?.js);
 
 // ################################ Оператор опциональной цепочки (.) ES11. ################################\
-const boxesQuery = document.querySelectorAll('.box');
-const boxesGet = document.getElementsByClassName('box');
+// const boxesQuery = document.querySelectorAll('.box');
+// const boxesGet = document.getElementsByClassName('box');
 
-boxesQuery.forEach(box => {
-    if(box.matches('.this')) {
-        console.log(box);
-    }
-});
+// boxesQuery.forEach(box => {
+//     if(box.matches('.this')) {
+//         console.log(box);
+//     }
+// });
 
-console.log(boxesQuery[0].closest('.wrapper'));
+// console.log(boxesQuery[0].closest('.wrapper'));
 
 
 
-// boxesQuery[0].remove();
-// boxesGet[0].remove();
+// // boxesQuery[0].remove();
+// // boxesGet[0].remove();
 
-// for (let i = 0; i < 5; i++) {
-//     const div = document.createElement('div');
-//     div.classList.add('box');
-//     document.body.append(div);
-// }
+// // for (let i = 0; i < 5; i++) {
+// //     const div = document.createElement('div');
+// //     div.classList.add('box');
+// //     document.body.append(div);
+// // }
 
-// console.log(boxesQuery);
-// console.log(boxesGet);
-// // console.log(document.body.children);
+// // console.log(boxesQuery);
+// // console.log(boxesGet);
+// // // console.log(document.body.children);
 
-// console.log(Array.from(boxesGet));
+// // console.log(Array.from(boxesGet));
+
+// ################################ Тип данных Symbol ################################\
+// // let id = Symbol('id');
+
+// // const obj = {
+// //     'name': 'Test',
+// //     [Symbol("id")]: 1,
+// //     // getId: function() {
+// //     //     return this[id];
+// //     // }
+// // };
+
+// // // let id = Symbol('id');
+// // // let id2 = Symbol('id');
+
+// // // console.log(id == id2);
+
+// // // obj[id] = 1;
+
+// // // console.log(obj.getId());
+
+// // console.log(obj[Object.getOwnPropertySymbols(obj)[0]]);
+
+// // // for (let value in obj) {
+// // //     console.log(value);
+// // // }
+
+// const myAwesomeDB = {
+//     movies: [],
+//     actors: [],
+//     [Symbol.for('id')]: 123
+// };
+
+// // Сторонний код библиотеки
+
+// myAwesomeDB.id = '323232';
+// console.log(myAwesomeDB[Symbol.for('id')]);
+// console.log(myAwesomeDB);
